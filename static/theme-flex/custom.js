5bad0074f3f004d0e1eac378ef3494e8d420de43
Ben Mansheim <github@mansheim.com>
2018-10-11 16:25:04 +0300

    Heading, Hamburger, Footer and more (#31)

--- static/theme-flex/custom.js
+++ static/theme-flex/custom.js
@@ -1,16 +1,37 @@
 jQuery('document').ready(function($) {
     var toc = $('#toc');
-    var distance = toc.offset().top - 100,
-    $window = $(window);
+    if (toc.length > 0) {
+        var distance = toc.offset().top - 100,
+        $window = $(window);
 
-    $window.scroll(function() {
-        if ( $window.scrollTop() >= distance ) {
-            // Your div has reached the top
-            toc.addClass('fixed-pos')
-        }
-        else {
-            toc.removeClass('fixed-pos')
-        }
+        $window.scroll(function() {
+            if ( $window.scrollTop() >= distance ) {
+                // Your div has reached the top
+                toc.addClass('fixed-pos')
+            }
+            else {
+                toc.removeClass('fixed-pos')
+            }
+        });
+    }
+    $('.hamburger').click(function() {
+        var w = $(window).width(),
+            nav = $('#navModal');
+        console.log(w/2);
+        // nav.css('width' , w/2);
+        $(this).toggleClass('is-active');
+        nav.slideToggle(300);
     });
 
+
+$('.main-content-right .nav li a').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 80
+    }, 750, 'swing');
 });
+
+
+});
+
