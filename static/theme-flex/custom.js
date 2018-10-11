0bd35be6135f4e1e6a14d8a66798b9bb12668b1b
Roee Yossef <roeeyossef@gmail.com>
2018-10-11 16:08:25 +0300

    general fix's

--- static/theme-flex/custom.js
+++ static/theme-flex/custom.js
@@ -23,15 +23,6 @@ jQuery('document').ready(function($) {
         nav.slideToggle(300);
     });
 
-    $('.hamburger').click(function() {
-        var w = $(window).width(),
-            nav = $('#navModal');
-        console.log(w/2);
-        // nav.css('width' , w/2);
-        $(this).toggleClass('is-active');
-        nav.slideToggle(300);
-    });
-
-
+// $('.main-content-right .nav li a').on('click', function())
 
 });
