dac3fd7eedcbda311ae6fb9dd72ee9349f9996b4
Roee Yossef <roeeyossef@gmail.com>
2018-10-15 16:57:36 +0300

    fix

--- static/theme-flex/custom.js
+++ static/theme-flex/custom.js
@@ -39,13 +39,38 @@ jQuery('document').ready(function($) {
     //     nav.slideToggle(300);
     // });
 
-	$('.main-content-right .nav li a').on('click', function (e) {
-		e.preventDefault();
 
-		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top - 80
-		}, 620, 'linear');
-	});
+	// Add smooth scrolling to all links
+	// $(".main-content-right .nav li a").on('click', function(event) {
+
+	// // Make sure this.hash has a value before overriding default behavior
+	// if (this.hash !== "") {
+	// 	// Prevent default anchor click behavior
+	// 	event.preventDefault();
+
+	// 	// Store hash
+	// 	var hash = this.hash;
+
+	// 	// Using jQuery's animate() method to add smooth page scroll
+	// 	// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	// 	$('html, body').animate({
+	// 	scrollTop: $(hash).offset().top - 80
+	// 	}, 630, function(){
+	
+	// 	// Add hash (#) to URL when done scrolling (default click behavior)
+	// 	window.location.hash = hash;
+	// 	});
+	// } // End if
+	// });
+
+
+	// $('.main-content-right .nav li a').on('click', function (e) {
+	// 	e.preventDefault();
+
+	// 	$('html, body').animate({
+	// 		scrollTop: $($(this).attr('href')).offset().top - 80
+	// 	}, 620, 'linear');
+	// });
 
 
 	function mobile_nav() {
