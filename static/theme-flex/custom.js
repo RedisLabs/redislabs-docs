728402645dabfb12eadc3fd6f3493d42b4ec9554
Roee Yossef <roeeyossef@gmail.com>
2018-10-22 14:55:07 +0300

    add header search functionality and optimizing header image

--- static/theme-flex/custom.js
+++ static/theme-flex/custom.js
@@ -36,6 +36,19 @@ jQuery('document').ready(function($) {
     });
 
 
+	$('.shortcuts li:nth-child(3) a').on('click', function (e) {
+		e.preventDefault();
+        console.log('clicked');
+        $('.search-box').slideToggle().toggleClass('active');
+        return false;
+	});
+
+	$('a.close.button').on('click', function (e) {
+        e.preventDefault();
+        console.log('clicked');
+        $('.search-box').slideToggle().toggleClass('active');
+    });
+
 
 
     $('.main-content-right .nav li a').on('click', function (e) {
