be9861d50e70ec64220863ea02bda9257f96216b
Harun Djulic <harundulic@gmail.com>
2019-04-25 09:48:05 +0200

    WIP

--- static/theme-flex/script.js
+++ static/theme-flex/script.js
@@ -98,7 +98,7 @@ jQuery(document).ready(function() {
     $("h1,h2,h1~h2,h1~h3,h1~h4,h1~h5,h1~h6,h2~h3,h3~h4,h4~h5").append(function (index, html) {
         var element = $(this);
         var url = document.location.origin + document.location.pathname;
-        var link = url + "#" + element[0].id;
+        var link = url + "#" + element[0].id; //try to make it the same as hugo's urlize
         return " <span class='anchor' data-clipboard-text='" + link + "'>"  +
             "</span>";
     });
