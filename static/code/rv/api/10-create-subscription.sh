0ee551a4d1aaad0644326bd1ab630fb7e30e1795
Danny Cohen <danny.cohen@outlook.com>
2019-05-02 20:05:08 +0300

    test code exmaple

--- static/code/rv/api/10-create-subscription.sh
+++ static/code/rv/api/10-create-subscription.sh
@@ -1,4 +1,75 @@
-curl -X GET "https://$HOST/database-modules" \
+TASK_ID=$(curl -X POST "https://$HOST/subscriptions" \
+    --header "Content-Type: application/json" \
     -H "accept: application/json" \
     -H "x-api-key: $ACCOUNT_KEY" \
-    -H "x-api-secret-key: $SECRET_KEY"
+    -H "x-api-secret-key: $SECRET_KEY" \
+    -d '
+{
+  "name": "My new subscription 10",
+  "dryRun": false,
+  "paymentMethodId": 8240,
+  "memoryStorage": "RAM",
+  "persistentStorageEncryption": true,
+  "cloudProviders": [
+    {
+      "provider": "AWS",
+      "cloudAccountId": 7033,
+      "regions": [
+        {
+          "region": "us-east-1",
+          "multipleAvailabilityZones": false,
+          "preferredAvailabilityZones": [
+            "us-east-1b"
+          ],
+          "networking": {
+            "deploymentCIDR": "10.0.0.0/24",
+            "vpcId": null
+          }
+        }
+      ]
+    }
+  ],
+  "databases": [
+    {
+      "name": "Redis-database-example",
+      "protocol": "redis",
+      "provider": "AWS",
+      "region": "us-east-1",
+      "memoryLimitInGb": 10,
+      "supportOSSClusterApi": true,
+      "dataPersistence": "none",
+      "replication": true,
+      "throughputMeasurement": {
+        "by": "operations-per-second",
+        "value": 10000
+      },
+      "modules": [
+        {
+          "databaseModuleName": "ReJSON",
+          "parameters": {}
+        }
+      ],
+      "quantity": 1,
+      "averageItemSizeInBytes": 1024
+    }
+  ]
+}
+' | jq -r .taskId )
+
+echo "TASK_ID=$TASK_ID"
+
+sleep 5 # seconds
+
+curl -X GET "https://$HOST/tasks/$TASK_ID" \
+    -H "accept: application/json" \
+    -H "x-api-key: $ACCOUNT_KEY" \
+    -H "x-api-secret-key: $SECRET_KEY" \
+    | jq -r .status
+
+sleep 5 # seconds
+
+curl -X GET "https://$HOST/tasks/$TASK_ID" \
+    -H "accept: application/json" \
+    -H "x-api-key: $ACCOUNT_KEY" \
+    -H "x-api-secret-key: $SECRET_KEY" \
+    | jq 
