f4f2236e6ab16aa082e055f1c630a6ac08030073
Danny Cohen <danny.cohen@outlook.com>
2019-05-05 11:17:50 +0300

    fix create subscription code sample

--- static/code/rv/api/10-create-subscription.sh
+++ static/code/rv/api/10-create-subscription.sh
@@ -3,61 +3,10 @@ TASK_ID=$(curl -s -X POST "https://$HOST/subscriptions" \
     -H "accept: application/json" \
     -H "x-api-key: $ACCOUNT_KEY" \
     -H "x-api-secret-key: $SECRET_KEY" \
-    -d '
-{
-  "name": "My new subscription 12",
-  "dryRun": true,
-  "paymentMethodId": 8240,
-  "memoryStorage": "RAM",
-  "persistentStorageEncryption": true,
-  "cloudProviders": [
-    {
-      "provider": "AWS",
-      "cloudAccountId": 7033,
-      "regions": [
-        {
-          "region": "us-east-1",
-          "multipleAvailabilityZones": false,
-          "preferredAvailabilityZones": [
-            "us-east-1b"
-          ],
-          "networking": {
-            "deploymentCIDR": "10.0.0.0/24",
-            "vpcId": null
-          }
-        }
-      ]
-    }
-  ],
-  "databases": [
-    {
-      "name": "Redis-database-example",
-      "protocol": "redis",
-      "provider": "AWS",
-      "region": "us-east-1",
-      "memoryLimitInGb": 10,
-      "supportOSSClusterApi": true,
-      "dataPersistence": "none",
-      "replication": true,
-      "throughputMeasurement": {
-        "by": "operations-per-second",
-        "value": 10000
-      },
-      "modules": [
-        {
-          "databaseModuleName": "ReJSON",
-          "parameters": {}
-        }
-      ],
-      "quantity": 3,
-      "averageItemSizeInBytes": 1000000
-    }
-  ]
-}
-' | jq -r .taskId )
+    --data-binary "@./static/code/rv/api/100-create-subscription-basic.json" \
+    | jq -r .taskId )
 
 echo "TASK_ID=$TASK_ID"
-
 
 STATUS=""
 
@@ -72,11 +21,7 @@ do
     echo "STATUS=$STATUS"
 done
 
-curl -s -X GET "https://$HOST/tasks/$TASK_ID" \
-    -H "accept: application/json" \
-    -H "x-api-key: $ACCOUNT_KEY" \
-    -H "x-api-secret-key: $SECRET_KEY" \
-    | jq -r .status
+echo "Response: "
 
 curl -s -X GET "https://$HOST/tasks/$TASK_ID" \
     -H "accept: application/json" \
