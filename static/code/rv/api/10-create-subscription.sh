634529ca9c1b1c668be1521e95674fa31de88d54
Danny Cohen <danny.cohen@outlook.com>
2019-05-07 23:04:00 +0300

    create subscription script fixes

--- static/code/rv/api/10-create-subscription.sh
+++ static/code/rv/api/10-create-subscription.sh
@@ -14,8 +14,8 @@ echo "TASK_ID=$TASK_ID"
 
 echo "Step 2: wait for processing completion"
 
-STATUS=
-while [ "$STATUS" != 'processing-completed' ] || [ "$STATUS" != 'processing-error' ]
+STATUS=received
+while [ "$STATUS" == "processing-in-progress" ] || [ "$STATUS" == "received" ]
 do
     sleep 3 # seconds   
     STATUS=$(curl -s -X GET "https://$HOST/tasks/$TASK_ID" \
@@ -26,14 +26,14 @@ do
     echo "STATUS=$STATUS"
 done
 
-echo "Step 3: print the response, containing the create resource Id"
+echo "Step 3: print the response, containing the created resource Id / error" 
 
 echo "Response: "
 curl -s -X GET "https://$HOST/tasks/$TASK_ID" \
     -H "accept: application/json" \
     -H "x-api-key: $ACCOUNT_KEY" \
     -H "x-api-secret-key: $SECRET_KEY" \
-    | jq -r .
+    | jq -r .response
 
 
 
