652c21512d2977550e347b4b04f4d6f2101657a7
Danny Cohen <danny.cohen@outlook.com>
2019-06-02 21:51:19 +0300

    backup database

--- static/code/rv/api/12-backup-database.sh
+++ static/code/rv/api/12-backup-database.sh
@@ -2,5 +2,4 @@ curl -s -X POST "https://$HOST/subscriptions/$SUBSCRIPTION_ID/databases/$DATABAS
     --header "Content-Type: application/json" \
     -H "accept: application/json" \
     -H "x-api-key: $ACCOUNT_KEY" \
-    -H "x-api-secret-key: $SECRET_KEY" \
-    | jq -r . 
+    -H "x-api-secret-key: $SECRET_KEY" 
