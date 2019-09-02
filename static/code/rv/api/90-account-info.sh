0922b570a971658638514d0587e2d9025786b223
Danny Cohen <danny.cohen@outlook.com>
2019-09-02 11:09:44 +0300

    fix typo in script

--- static/code/rv/api/90-account-info.sh
+++ static/code/rv/api/90-account-info.sh
@@ -1,4 +1,4 @@
-curl -X GET "https://$HOST/subscriptions/" \
+curl -X GET "https://$HOST/" \
     -H "accept: application/json" \
     -H "x-api-key: $ACCOUNT_KEY" \
     -H "x-api-secret-key: $SECRET_KEY"
