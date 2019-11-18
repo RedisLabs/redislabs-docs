17efc76c79d548e32517abb404165742e1bbe849
Danny Cohen <danny.cohen@outlook.com>
2019-11-18 10:37:48 +0200

    remove beta mentions

--- static/code/rv/api/07-set-variables-with-subscription-and-database-id.sh
+++ static/code/rv/api/07-set-variables-with-subscription-and-database-id.sh
@@ -1,4 +1,4 @@
-export HOST=api-beta.redislabs.com/beta1
+export HOST=api.redislabs.com/v1.redislabs.com/beta1
 export ACCOUNT_KEY={replace-with-your-account-key}
 export SECRET_KEY={replace-with-your-secret-key}
 export SUBSCRIPTION_ID={subscription-id}
