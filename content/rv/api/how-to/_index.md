f0ea59224b5f9e7adffce8b59361150711ae088c
Ben Mansheim <ben@redislabs.com>
2019-11-18 16:27:31 +0200

    API content in docs, code and images

---
+++ content/rv/api/how-to/_index.md
@@ -0,0 +1,8 @@
+---
+Title: How To
+description:
+weight: 30
+alwaysopen: false
+categories: ["RC Pro"]
+---
+{{%allchildren style="h2" description="true"%}}
---
+++ static/code/rv/api/10-create-subscription.sh
@@ -0,0 +1,30 @@
+TASK_ID=$(curl -s -X POST "https://$HOST/subscriptions" \
+    --header "Content-Type: application/json" \
+    -H "accept: application/json" \
+    -H "x-api-key: $ACCOUNT_KEY" \
+    -H "x-api-secret-key: $SECRET_KEY" \
+    --data-binary "@./static/code/rv/api/100-create-subscription-basic.json" \
+    | jq -r .taskId )
+
+echo "TASK_ID=$TASK_ID"
+
+STATUS=""
+
+while [ "$STATUS" != "processing-completed" ]
+do
+    sleep 3 # seconds   
+    STATUS=$(curl -s -X GET "https://$HOST/tasks/$TASK_ID" \
+    -H "accept: application/json" \
+    -H "x-api-key: $ACCOUNT_KEY" \
+    -H "x-api-secret-key: $SECRET_KEY" \
+    | jq -r .status)
+    echo "STATUS=$STATUS"
+done
+
+echo "Response: "
+
+curl -s -X GET "https://$HOST/tasks/$TASK_ID" \
+    -H "accept: application/json" \
+    -H "x-api-key: $ACCOUNT_KEY" \
+    -H "x-api-secret-key: $SECRET_KEY" \
+    | jq -r .response
