6fd538ebf264f717bcd70d63c50001cc647a2690
Andrew Brookins <andrew.brookins@redislabs.com>
2020-09-11 09:19:37 -0700

    WIP on new autocomplete

--- static/js/search.js
+++ static/js/search.js
@@ -1,59 +1,111 @@
-const SEARCH_API_URL = "http://docs.andrewbrookins.com:8001/search"
+// const SEARCH_API_URL = "https://docsearch-dot-redislabs-university.appspot.com/search"
+const SEARCH_API_URL = "http://localhost:8081/search"
+const wikiUrl = 'https://en.wikipedia.org'
+const params = 'action=query&list=search&format=json&origin=*'
+const THIRTY_SECONDS = 30000
 
-/**
- * Trigger a search and transform the result
- *
- * @param  {String} query
- * @return {Array}  results
- */
-function search(query) {
+function setWithExpiry(key, value, ttl) {
+	const now = new Date()
+
+	const item = {
+		value: value,
+		expiry: now.getTime() + ttl,
+	}
+	localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(key) {
+  return
+	const itemStr = localStorage.getItem(key)
+	if (!itemStr) {
+		return null
+	}
+	const item = JSON.parse(itemStr)
+	const now = new Date()
+
+	if (now.getTime() > item.expiry) {
+		localStorage.removeItem(key)
+		return null
+	}
+	return item.value
+}
+
+
+new Autocomplete('#autocomplete', {
+  debounceTime: 2,
+
+  search: input => {
+    const url = SEARCH_API_URL + "?q=" + input.trim() + "*"
+
+    if (input.length === 0) {
+      return []
+    }
+
     try { xhr.abort(); } catch(e){}
-    return $.getJSON(SEARCH_API_URL + "?q=" + query + "*")
+
+    const cachedResults = getWithExpiry(url)
+
+    if (cachedResults) {
+      return cachedResults
+    }
+
+    return new Promise(resolve => {
+      $.getJSON(url)
         .fail(function(jqxhr, textStatus, error) {
             var err = textStatus + ", " + error;
             console.error("Error querying search API:", err);
-        });
-}
+            resolve([])
+        })
+        .done(function(data) {
+          setWithExpiry(url, data.results, THIRTY_SECONDS)
+          resolve(data.results)
+        })
+    })
+  },
 
-// Let's get started
-$( document ).ready(function() {
-    var searchList = new autoComplete({
-        delay: 0,
-        minChars: 2,
-        /* selector for the search box element */
-        selector: $("#search-by").get(0),
-        /* source is the callback to perform the search */
-        source: function(term, response) {
-            search(term).done(function(data) {
-                console.log(data.results);
-                response(data.results);
-            });
-        },
-        /* renderItem displays individual search results */
-        renderItem: function(item, term) {
-            var numContextWords = 3;
-            var regEx = "(?:\\s?(?:[\\w\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~]+)\\s?){0";
-            var text = item.body.match(
-                regEx+numContextWords+"}" +
-                    term+regEx+numContextWords+"}");
-            if(text && text.length > 0) {
-                var len = text[0].split(' ').length;
-                item.context = len > 1? '...' + text[0].trim() + '...' : null;
-            }
-            item.cat = item.root_page;
-            return '<div class="autocomplete-suggestion" ' +
-                'data-term="' + term + '" ' +
-                'data-title="' + item.title + '" ' +
-                'data-uri="'+ item.url + '?s=' + term + '"' +
-                'data-context="' + item.section_title + '">' +
-                    '<div>' + item.title + '<strong class="category">' + item.root_page + '</strong> </div>' +
-                    '<div class="context">' + item.section_title +'</div>' +
-                '</div>';
-        },
-        /* onSelect callback fires when a search suggestion is chosen */
-        onSelect: function(e, term, item) {
-            console.log(item.getAttribute('data-val'));
-            location.href = item.getAttribute('data-uri');
-        }
-    });
-});
+  renderResult: (result, props) => {
+    const root = result.hierarchy[0]
+
+    if (result.section_title) {
+      return `
+        <li ${props}>
+          <div class="search-root">
+            ${root}
+          </div>
+          <div class="search-title">
+            ${result.title}
+          </div>
+          <div class="search-section-title">
+            ${result.section_title}
+          </div>
+          <div class="search-snippet">
+            ${result.body}
+          </div>
+        </li>
+      `
+    } else {
+      return `
+        <li ${props}>
+          <div class="search-root">
+            ${root}
+          </div>
+          <div class="search-title">
+            ${result.title}
+          </div>
+          <div class="search-body">
+            ${result.body}
+          </div>
+        </li>
+      `
+    }
+  },
+
+  // Open the selected article in
+  // a new window
+  onSubmit: result => {
+    if (result) {
+      window.open(`${result.url}`)
+    }
+  }
+})
+
