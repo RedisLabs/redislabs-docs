1af95c542b96e5942d9cb97ab456a3e0d0616a35
Andrew Brookins <andrew.brookins@redislabs.com>
2020-10-09 12:13:10 -0700

    Fix Enter key with no selection; style changes for inline search

--- static/js/search.js
+++ static/js/search.js
@@ -1,129 +1,187 @@
-const SEARCH_API_URL = "https://search-service.redislabs.com/search"
-const THIRTY_SECONDS = 30000
+(function() {
+
+  const SEARCH_API_URL = "https://search-service.redislabs.com/search"
+  const THIRTY_SECONDS = 30000
 
 
-function setWithExpiry(key, value, ttl) {
-	const now = new Date()
+  function setWithExpiry(key, value, ttl) {
+    const now = new Date()
 
-	const item = {
-		value: value,
-		expiry: now.getTime() + ttl,
-	}
-	localStorage.setItem(key, JSON.stringify(item))
-}
-
-
-function getWithExpiry(key) {
-	const itemStr = localStorage.getItem(key)
-	if (!itemStr) {
-		return null
-	}
-	const item = JSON.parse(itemStr)
-	const now = new Date()
-
-	if (now.getTime() > item.expiry) {
-		localStorage.removeItem(key)
-		return null
-	}
-
-	return item.value
-}
+    const item = {
+      value: value,
+      expiry: now.getTime() + ttl,
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+  }
 
 
-new Autocomplete('#autocomplete', {
-  debounceTime: 2,
+  function getWithExpiry(key) {
+    const itemStr = localStorage.getItem(key)
+    if (!itemStr) {
+      return null
+    }
+    const item = JSON.parse(itemStr)
+    const now = new Date()
 
-  search: input => {
-    const trimmedInput = input.trim()
-    const url = `${SEARCH_API_URL}?q=${trimmedInput}*`
-
-    if (input.length === 0) {
-      return []
+    if (now.getTime() > item.expiry) {
+      localStorage.removeItem(key)
+      return null
     }
 
-    try { xhr.abort(); } catch(e){}
+    return item.value
+  }
 
-    // Save the query so we can append it to a selected result URL later.
-    // We use this to track search queries.
-    this.lastQuery = trimmedInput
+  class RedisLabsAutocomplete extends Autocomplete {
+    constructor(
+      root,
+      opts = {}
+    ) {
+      super(root, opts)
 
-    const cachedResults = getWithExpiry(url)
-
-    if (cachedResults) {
-      return cachedResults
+      this.input.removeEventListener('keydown', this.core.handleKeyDown)
+      this.input.addEventListener('keydown', e => this.handleKeyDown(e))
     }
 
-    return new Promise(resolve => {
-      $.getJSON(url)
-        .fail(function(jqxhr, textStatus, error) {
-          const err = `${textStatus}, ${error}`
-          console.error("Error querying search API:", err)
-          resolve([])
-        })
-        .done(function(data) {
-          setWithExpiry(url, data.results, THIRTY_SECONDS)
-          resolve(data.results)
-        })
-    })
-  },
+    handleKeyDown(event) {
+      const { key } = event
 
-  renderResult: (result, props) => {
-    const root = result.hierarchy[0]
+      switch (key) {
+        case 'Up': // IE/Edge
+        case 'Down': // IE/Edge
+        case 'ArrowUp':
+        case 'ArrowDown': {
+          const selectedIndex =
+            key === 'ArrowUp' || key === 'Up'
+              ? this.core.selectedIndex - 1
+              : this.core.selectedIndex + 1
+          event.preventDefault()
+          this.core.handleArrows(selectedIndex)
+          break
+        }
+        case 'Tab': {
+          this.core.selectResult()
+          break
+        }
+        case 'Enter': {
+          const selectedResult = this.core.results[this.core.selectedIndex]
 
-    if (result.section_title) {
-      return `
-        <li ${props}>
-          <div class="search-root">
-            ${root}
-          </div>
-          <div class="search-left">
-            <div class="search-title">
-              ${result.title}
-            </div>
-          </div>
-          <div class="search-right">
-            <div class="search-section-title">
-              ${result.section_title}
-            </div>
-            <div class="search-body">
-              ${result.body}
-            </div>
-          </div>
-        </li>
-      `
-    } else {
-      return `
-        <li ${props}>
-          <div class="search-root">
-            ${root}
-          </div>
-          <div class="search-left">
-            <div class="search-title">
-              ${result.title}
-            </div>
-          </div>
-          <div class="search-right">
-            <div class="search-section-title">
-              ${result.title}
-            </div>
-            <div class="search-body">
-              ${result.body}
-            </div>
-          </div>
-        </li>
-      `
-    }
-  },
+          // Avoid closing the search box on Enter if a result is not selected.
+          if (!selectedResult) {
+            return;
+          }
 
-  getResultValue: result => "",
-
-  // Open the selected article in
-  // a new window
-  onSubmit: result => {
-    if (result) {
-      const lastQuery = encodeURIComponent(this.lastQuery)
-      window.open(`${result.url}?s=${lastQuery}`)
+          this.core.selectResult()
+          this.core.onSubmit(selectedResult)
+          break
+        }
+        case 'Esc': // IE/Edge
+        case 'Escape': {
+          this.core.hideResults()
+          this.core.setValue()
+          break
+        }
+        default:
+          return
+      }
     }
   }
-})
 
+
+  new RedisLabsAutocomplete('#autocomplete', {
+    debounceTime: 2,
+
+    search: input => {
+      const trimmedInput = input.trim()
+      const url = `${SEARCH_API_URL}?q=${trimmedInput}*`
+
+      if (input.length === 0) {
+        return []
+      }
+
+      try { xhr.abort(); } catch(e){}
+
+      // Save the query so we can append it to a selected result URL later.
+      // We use this to track search queries.
+      this.lastQuery = trimmedInput
+
+      const cachedResults = getWithExpiry(url)
+
+      if (cachedResults) {
+        return cachedResults
+      }
+
+      return new Promise(resolve => {
+        $.getJSON(url)
+          .fail(function(jqxhr, textStatus, error) {
+            const err = `${textStatus}, ${error}`
+            console.error("Error querying search API:", err)
+            resolve([])
+          })
+          .done(function(data) {
+            setWithExpiry(url, data.results, THIRTY_SECONDS)
+            resolve(data.results)
+          })
+      })
+    },
+
+    renderResult: (result, props) => {
+      const root = result.hierarchy[0]
+
+      if (result.section_title) {
+        return `
+          <li ${props}>
+            <div class="search-root">
+              ${root}
+            </div>
+            <div class="search-left">
+              <div class="search-title">
+                ${result.title}
+              </div>
+            </div>
+            <div class="search-right">
+              <div class="search-section-title">
+                ${result.section_title}
+              </div>
+              <div class="search-body">
+                ${result.body}
+              </div>
+            </div>
+          </li>
+        `
+      } else {
+        return `
+          <li ${props}>
+            <div class="search-root">
+              ${root}
+            </div>
+            <div class="search-left">
+              <div class="search-title">
+                ${result.title}
+              </div>
+            </div>
+            <div class="search-right">
+              <div class="search-section-title">
+                ${result.title}
+              </div>
+              <div class="search-body">
+                ${result.body}
+              </div>
+            </div>
+          </li>
+        `
+      }
+    },
+
+    getResultValue: result => "",
+
+    // Open the selected article in
+    // a new window
+    onSubmit: result => {
+      if (result) {
+        const lastQuery = encodeURIComponent(this.lastQuery)
+        window.open(`${result.url}?s=${lastQuery}`, "_top")
+      }
+    }
+  })
+
+})()
