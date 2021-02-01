c50b0d24441580691047ebdd74ce5927177b168a
Andrew Brookins <andrew.brookins@redislabs.com>
2021-02-01 15:00:34 -0800

    Point search API at live URL

--- static/js/search.js
+++ static/js/search.js
@@ -1,7 +1,6 @@
 (function() {
 
-  // const SEARCH_API_URL = "https://search-service.redislabs.com/search"
-  const SEARCH_API_URL = "http://localhost:8080/search"
+  const SEARCH_API_URL = "https://search-service.redislabs.com/search"
   const THIRTY_SECONDS = 30000
   const SEARCH_LOGO = '<a class="powered-by-redisearch" href="https://oss.redislabs.com/redisearch/"></a>'
 
