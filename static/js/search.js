9c52b215fc148e78641b545f4339567dfd192386
Andrew Brookins <andrew.brookins@redislabs.com>
2021-03-05 12:59:45 -0800

    Use production index (#1263)

--- static/js/search.js
+++ static/js/search.js
@@ -1,7 +1,7 @@
 (function() {
 
   const SEARCH_API_URL = "https://search-service.redislabs.com/search"
-  const SEARCH_SITE = "https://docs.redislabs.com/staging/boost-current-section"
+  const SEARCH_SITE = "https://docs.redislabs.com/latest"
   const THIRTY_SECONDS = 30000
   const SEARCH_LOGO = '<a class="powered-by-redisearch" href="https://oss.redislabs.com/redisearch/"></a>'
 
