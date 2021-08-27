3052e4c349db72449ab6927f88529e55af31f7fb
Rachel Elledge <rachel.elledge@redislabs.com>
2021-08-27 13:56:53 -0500

    Partial layouts and scripts for PDF generation

---
+++ utilities/create_pdf_tarball/create_html.sh
@@ -0,0 +1,28 @@
+echo "Preparing html for PDF generation..."
+
+mkdir ../../layouts/pdf-gen-rc-docs
+cat ./list_template_recursive.html | sed 's/parent_filepath/"rc\/_index.md"/g' > ../../layouts/pdf-gen-rc-docs/list.html
+mkdir ../../content/pdf-gen-rc-docs
+touch ../../content/pdf-gen-rc-docs/_index.md
+
+mkdir ../../layouts/pdf-gen-rs-docs
+cat ./list_template_recursive.html | sed 's/parent_filepath/"rs\/_index.md"/g' > ../../layouts/pdf-gen-rs-docs/list.html
+mkdir ../../content/pdf-gen-rs-docs
+touch ../../content/pdf-gen-rs-docs/_index.md
+
+mkdir ../../layouts/pdf-gen-ri-docs
+cat ./list_template_recursive.html | sed 's/parent_filepath/"ri\/_index.md"/g' > ../../layouts/pdf-gen-ri-docs/list.html
+mkdir ../../content/pdf-gen-ri-docs
+touch ../../content/pdf-gen-ri-docs/_index.md
+
+mkdir ../../layouts/pdf-gen-modules-docs
+cat ./list_template_recursive.html | sed 's/parent_filepath/"modules\/_index.md"/g' > ../../layouts/pdf-gen-modules-docs/list.html
+mkdir ../../content/pdf-gen-modules-docs
+touch ../../content/pdf-gen-modules-docs/_index.md
+
+mkdir ../../layouts/pdf-gen-platforms-docs
+cat ./list_template_recursive.html | sed 's/parent_filepath/"platforms\/_index.md"/g' > ../../layouts/pdf-gen-platforms-docs/list.html
+mkdir ../../content/pdf-gen-platforms-docs
+touch ../../content/pdf-gen-platforms-docs/_index.md
+
+echo "Finished preparing html for PDF generation."
