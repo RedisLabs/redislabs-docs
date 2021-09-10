7fd00d537984ec83aacc92235a1286e70a80ce16
Rachel Elledge <rachel.elledge@redislabs.com>
2021-09-10 15:27:19 -0500

    Test to enable PDF internal links

---
+++ utilities/create_pdf_tarball/edit_links.sh
@@ -0,0 +1,7 @@
+echo "Generating internal links for offline docs PDF generation..."
+
+echo "Generating internal links for rs docs html"
+
+cat ../../public/pdf-gen-rs-docs/index.html | sed 's^href="../rs/getting-started/getting-started-docker/^href="#Getting%20Started%20with%20Redis%20Enterprise%20Software%20using%20Docker"^g' > ../../public/pdf-gen-rs-docs/index.html
+
+echo "Finished generating internal links for offline docs PDF generation."
